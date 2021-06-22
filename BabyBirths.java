d55664138bd2cb8b695d346e17e208ee23c1b73e
okikechinonso <Chinonsookike@gmail.com>
2021-06-22 08:02:25 +0100

    everything is working except for two method

--- BabyBirths.java
+++ BabyBirths.java
@@ -6,37 +6,32 @@
 import edu.duke.*;
 import org.apache.commons.csv.*;
 import java.io.*;
-public class BabyBirths {
-    
-    // this print total number of births, girls and boys
-    public void totalBirths (FileResource fr) {
+public class BabyBirths {    
+    // this print total number of births, girls or boys
+    public int totalBirths (FileResource fr,String gender) {
         int totalBirths = 0;
-        int totalBoys = 0;
-        int totalGirls = 0;
-        int countB=0, countG=0;
-        
-        
         for (CSVRecord rec : fr.getCSVParser(false)) {
-            int numBorn = Integer.parseInt(rec.get(2));
-            totalBirths += numBorn;
-            if (rec.get(1).equals("M")) {
-                totalBoys += numBorn;
-                countB++;
-            }
-            else {
-                totalGirls += numBorn;
-                countG++;
+            int numBorn = Integer.parseInt(rec.get(2));            
+            if (rec.get(1).equals(gender)) {
+                totalBirths += numBorn;  
+            }            
+        }
+        return totalBirths;
+    }
+    public int getBirthNum(FileResource fr,String name, String gender){
+        int birthNum = 0;
+        for(CSVRecord rec : fr.getCSVParser(false)){
+            String csvName = rec.get(0);
+            String csvGender = rec.get(1);
+            if(csvName.equals(name) && csvGender.equals(gender)){
+                birthNum = Integer.parseInt(rec.get(2));
             }
         }
-        System.out.println("total births = " + totalBirths);
-        System.out.println("female girls = " + totalGirls
-        +" The number of boys burn is " +countG);
-        System.out.println("male boys = " + totalBoys
-        + " The number of boys burn is " +countB);
+        
+        return birthNum;
     }
     // this get the rank name
-    public int getRank(int year, String name, String gender){
-        int genderCount = 0;
+    public int getRank(int year, String name, String gender){        
         int totalBirth = 0;
         int rank = 1;
         
@@ -58,7 +53,31 @@ public class BabyBirths {
             return -1;
         }
         
-    return rank;
+    return rank; 
+    }
+    public int getRankMany(FileResource fr, int year, String name, String gender){        
+        int totalBirth = 0;
+        int rank = 1;
+        
+        fr = yearFile(year);
+        
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            int currBirth = Integer.parseInt(rec.get(2));
+            String csvName = rec.get(0);
+            String csvGender = rec.get(1);           
+            
+            if(csvName.equals(name) && csvGender.equals(gender)){
+                totalBirth = Integer.parseInt(rec.get(2));
+            }
+            if(csvGender.equals(gender) && totalBirth<currBirth) rank++;                        
+            
+        }
+        //check if the a certain name exist
+        if(totalBirth==0){
+            return -1;
+        }
+        
+    return rank; 
     }
     public String getName(int year, int rank, String gender){
         int count = 0;
@@ -83,24 +102,123 @@ public class BabyBirths {
         return newName;
     }
     public int yearOfHighestRank(String name, String gender){
+        String fileName,yearString;
+         
+        int year= 0,prevYear = 0;
+        int index = 0;
+        int currRank = 0, prevRank=0;
+        DirectoryResource dr = new DirectoryResource();
+        for(File f: dr.selectedFiles()){
+            
+            fileName = f.getName();
+            yearString = fileName.substring(3,7);
+            year =Integer.parseInt(yearString);            
+            
+            FileResource fr = new FileResource(f); 
+            
+            currRank = getRankMany(fr, year, name, gender);            
+            
+            if(prevRank == 0){                
+                prevRank=currRank;
+                prevYear=year;                               
+            }
+            else{                
+                if(currRank<prevRank){                       
+                        prevYear = year;                       
+                        prevRank=currRank;
+                    }                
+                else{                 
+                        if(currRank > prevRank){                    
+                            prevRank = prevRank;
+                            prevYear = prevYear;
+                        }
+                        else{
+                            if(currRank > prevRank && prevRank == prevRank){
+                                prevRank=prevRank;
+                            }
+                            else{
+                                return prevYear;
+                            }
+                        }
+                    return prevYear;
+                }
+                return prevYear;
+            }
+            
+        }
+        return -1;
+    }
+    public double getAverageRank(String name, String gender){
+        String fileName;
+        String yearString;
+        int year= 0;
+        int currRank = 0;
+        int rankSum = 0;
+        int count = 0;
+        DirectoryResource dr = new DirectoryResource();
+        for(File f: dr.selectedFiles()){
+            count++;
+            fileName = f.getName();
+            yearString = fileName.substring(3,7);
+            year =Integer.parseInt(yearString);            
+            
+            FileResource fr = new FileResource(f); 
+            
+            currRank = getRankMany(fr, year, name, gender);
+            rankSum += currRank;
+        }
+        return (double)rankSum/count;
+    }
+    public int getTotalBirthRankedHigher(int num, String name, String gender){
+        String fileName = "";
+        String copyFile;
+        String yearString;
+        int year= 0;
+        int currRank = 0;
+        int prevRank = 0;
+        int count = 0;
+        int currBirth = 0;
         
         DirectoryResource dr = new DirectoryResource();
         for(File f: dr.selectedFiles()){
+            
+            fileName = f.getName();
+            
+            yearString = fileName.substring(3,7);
+            year =Integer.parseInt(yearString);            
+            
             FileResource fr = new FileResource(f);            
+            
+            currRank = getRankMany(fr, year, name, gender);
+            
+            if(currRank != 0 ){
+                currBirth = getBirthNum (fr,name, gender);
+            }         
+            
+            
         }
-        return 0;
+        System.out.println("currBirth"+currBirth);
+        FileResource frs = new FileResource("data/yob"+num+".csv");
+        
+        for (CSVRecord rec : frs.getCSVParser(false)){
+            int birth = Integer.parseInt(rec.get(2));
+            String csvGender = rec.get(1);
+            if( birth > currBirth && csvGender.equals(gender)){
+                count += birth;
+            }
+        }
+        return count;
     }
     public FileResource yearFile(int year){
         FileResource fr = new FileResource("data/yob"+year+".csv");
         return fr;
     }
     public void testTotalBirths () {
-        //FileResource fr = new FileResource();
         FileResource fr = new FileResource("data/yob2014.csv");
-        totalBirths(fr);
-        int count = getRank(2014,"Jacob","M");
-        String name = getName(2014,4,"M");
-        String newName = whatIsNameInYear("Jacob",2014,2013,"M");        
-        System.out.println(newName);
+        int num = getRank( 1960, "Emily", "F");
+        String name = getName(1982, 450,"M");        
+        
+        System.out.println(num);
+                
     }
 }
